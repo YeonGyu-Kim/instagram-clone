152cde227443763bda6978cdcd992de0b41e75d0
ygkim <ygkim527@naver.com>
2023-06-18 22:27:32 +0900

    Add: 포스트 리스트 데이터 연결 및 기본 정보 출력

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -1,3 +1,30 @@
+'use client';
+
+import useSWR from 'swr';
+import { MoonLoader } from 'react-spinners';
+import { SimplePost } from '../../sanity-studio/model/posts';
+import PostListCard from './PostListCard';
+
 export default function PostList() {
-  return <div>PostList</div>;
+  const { data: posts, isLoading: loading } =
+    useSWR<SimplePost[]>('/api/posts');
+
+  return (
+    <section>
+      {loading && (
+        <div>
+          <MoonLoader size={32} color='#F60485' />
+        </div>
+      )}
+      {posts && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post?.id}>
+              <PostListCard post={post} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
 }
