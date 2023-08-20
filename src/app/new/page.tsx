d3c9c5f52276c971b2159284d32f8f4c74efc9c9
ygkim <ygkim527@naver.com>
2023-08-20 22:55:07 +0900

    Add: 드래그, 클릭 시 이미지 등록 되도록 추가

---
+++ src/app/new/page.tsx
@@ -0,0 +1,19 @@
+import NewPost from '@/components/NewPost';
+import { Metadata } from 'next';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { authOptions } from '../api/auth/[...nextauth]/route';
+
+export const metaData: Metadata = {
+  title: 'New Post',
+  description: 'Create a new post',
+};
+
+export default async function Page() {
+  const session = await getServerSession(authOptions);
+  console.log(session);
+  if (!session?.user) {
+    redirect('/auth/signin');
+  }
+  return <NewPost user={session?.user}></NewPost>;
+}
