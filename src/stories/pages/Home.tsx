4093ef29f3d62200001882dfb3a9d43b0dd7e8d4
ygkim <ygkim527@naver.com>
2023-08-23 22:41:05 +0900

    Stroybook 셋업

---
+++ src/stories/pages/Home.tsx
@@ -0,0 +1,21 @@
+import { PulseLoader } from 'react-spinners';
+
+type Props = {
+  text: string;
+  onClick: () => void;
+  blue?: boolean;
+  isUpdating?: boolean;
+};
+
+export function Home({ text, onClick, blue = true, isUpdating }: Props) {
+  return (
+    <button
+      className={`rounded-md text-sm font-bold py-1.5 px-4 ${
+        blue ? 'bg-btn-blue' : 'bg-btn-gray'
+      }`}
+      onClick={() => onClick()}
+    >
+      {isUpdating ? <PulseLoader size={6} color='#EFEFEF' /> : text}
+    </button>
+  );
+}
