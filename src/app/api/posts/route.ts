5d15d4fb0afee14ef8111e23c80af63098e040bb
ygkim <ygkim527@naver.com>
2023-08-25 23:58:33 +0900

    Add: 새 포스트 작성 및 등록 작업

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,7 +1,8 @@
 import { getServerSession } from 'next-auth';
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { authOptions } from '../auth/[...nextauth]/route';
-import { getFollowingPostOf } from '@/service/posts';
+import { createPost, getFollowingPostOf } from '@/service/posts';
+import { getToken } from 'next-auth/jwt';
 
 export async function GET() {
   const session = await getServerSession(authOptions);
@@ -13,4 +14,27 @@ export async function GET() {
   }
 
   return getFollowingPostOf(username).then((data) => NextResponse.json(data));
+}
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  const user = session?.user;
+  const secret = process.env.NEXTAUTH_SECRET;
+  const token = await getToken({ req, secret });
+
+  if (!user) {
+    return new Response('Authentication Error', { status: 401 });
+  }
+
+  const form = await req.formData();
+  const text = form.get('text')?.toString();
+  const file = form.get('file') as Blob;
+
+  if (!text || !file) {
+    return new Response('Bad Request', { status: 400 });
+  }
+
+  return createPost(text, file, token?.sub).then((data) =>
+    NextResponse.json(data)
+  );
 }
