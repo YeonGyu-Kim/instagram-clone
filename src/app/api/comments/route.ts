fec893cce31f8b6273b5d4ea94ffaee172cae506
ygkim <ygkim527@naver.com>
2023-09-02 00:40:25 +0900

    Fix: api route 중복 코드 줄이기

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,24 +1,16 @@
-import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
-import { authOptions } from '../auth/[...nextauth]/route';
 import { addComment } from '@/service/posts';
-import { getToken } from 'next-auth/jwt';
+import { withSessionUser } from '@/util/session';
 
 export async function POST(req: NextRequest) {
-  const session = await getServerSession(authOptions);
-  const user = session?.user;
-  const secret = process.env.NEXTAUTH_SECRET;
-  const token = await getToken({ req, secret });
-  if (!user) {
-    return new Response('Authentication Error', { status: 401 });
-  }
+  return withSessionUser(async (sub) => {
+    const { id, comment } = await req.json();
 
-  const { id, comment } = await req.json();
-
-  if (!id || comment === undefined) {
-    return new Response('Bad Request', { status: 400 });
-  }
-  return addComment(id, comment, token?.sub)
-    .then((data) => NextResponse.json(data))
-    .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+    if (!id || comment === undefined) {
+      return new Response('Bad Request', { status: 400 });
+    }
+    return addComment(id, comment, sub)
+      .then((data) => NextResponse.json(data))
+      .catch((error) => new Response(JSON.stringify(error), { status: 500 }));
+  }, req);
 }
