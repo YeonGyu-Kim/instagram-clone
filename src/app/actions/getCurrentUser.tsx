20dcf2fa53bd6a096d9c9cca562b10ba4b1c3a39
ygkim <ygkim527@naver.com>
2023-09-19 23:26:15 +0900

    로그인 시 db에 유저가 없을 경우 유저 정보 추가

--- src/app/actions/getCurrentUser.tsx
+++ src/app/actions/getCurrentUser.tsx
@@ -1,16 +1,28 @@
 import prisma from '@/app/libs/prismadb';
 
-export default async function getCurrentUser(email: string) {
+export default async function getCurrentUser(
+  email: string,
+  name: string,
+  image: string
+) {
   try {
     const currentUser = await prisma.user.findUnique({
       where: {
         email,
       },
     });
-    if (!currentUser) {
+    if (!email) {
       return null;
     }
-    return currentUser;
+    if (!currentUser) {
+      await prisma.user.create({
+        data: {
+          email,
+          name,
+          image,
+        },
+      });
+    }
   } catch (error) {
     console.log(error);
     return null;
