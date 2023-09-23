2d2b4d1a564c20f3f3e6df2acc0212c41569dd60
ygkim <ygkim527@naver.com>
2023-09-23 21:35:21 +0900

    기존 사용자 검색 창 react hook form 적용

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -8,6 +8,11 @@ import UserCard from './UserCard';
 import useDebounce from '@/hooks/debounce';
 import useThrottle from '@/hooks/throttle';
 import { debounce } from 'lodash';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+type FormData = {
+  user: string;
+};
 
 export default function UserSearch() {
   const [keyword, setKeyword] = useState('');
@@ -18,18 +23,22 @@ export default function UserSearch() {
   const { handleDebounced } = useDebounce(setDebouncedValue);
   //const { handleThrottle } = useThrottle(setThrottledValue);
 
+  const { register, handleSubmit } = useForm<FormData>();
+
   const {
     data: users,
     isLoading,
     error,
   } = useSWR<SearchUser[]>(`/api/search/${debouncedValue}`);
 
-  const onSubmit = (e: React.FormEvent) => {
+  /* const onSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-  };
+  }; */
+
+  const onSubmit: SubmitHandler<FormData> = (data) => console.log(data);
   return (
     <section className='w-full max-w-md flex flex-col items-center p-6'>
-      <form onSubmit={onSubmit}>
+      {/*  <form onSubmit={onSubmit}>
         <input
           className='bg-border-gray outline-none px-4 py-2 mb-4 rounded-lg'
           type='text'
@@ -41,6 +50,23 @@ export default function UserSearch() {
             handleDebounced(e?.target.value);
             //handleThrottle(e?.target.value);
           }}
+        ></input>
+      </form> */}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          className='bg-border-gray outline-none px-4 py-2 mb-4 rounded-lg'
+          type='text'
+          autoFocus
+          placeholder='검색'
+          value={keyword}
+          {...register('user', {
+            required: true,
+            onChange: (e) => {
+              setKeyword(e?.target.value);
+              handleDebounced(e?.target.value);
+              //handleThrottle(e?.target.value);
+            },
+          })}
         ></input>
       </form>
       {isLoading && <GridSpinner />}
